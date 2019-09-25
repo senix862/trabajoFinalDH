dd57ef3b7c3d85b1a71d51265b32de3acf2ce160
senix862 <senix55@gmail.com>
2019-09-25 17:30:43 -0300

    Falta arreglar .php, mejorar el CSS y empezar a borrar archivos

---
+++ partials/footer.php
@@ -0,0 +1,25 @@
+<footer>
+  <section class="newsletter-redesSociales">
+    <!-- Redes Sociales -->
+      <h5>¡Síganos en nuestras redes sociales!</h5>
+      <div class="redesSociales">
+        <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+        <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+      </div>
+      <!-- Newsletter -->
+      <h5>Suscribirme para conocer mas novedades:</h5>
+      <div class="footer-Sociales">
+        <input id="E-MAIL" type="email" name="E-MAIL" value=""  placeholder="example@example.com" required>
+        <button id="subs" type="submit" name="button">
+            Suscribirse
+        </button>
+      </div>
+  </section>
+  <div class="cierre">
+      <a href="contacto.html">CONTACTO</a>
+      <a href="faq.html">PREGUNTAS FRECUENTES</a>
+  </div>
+
+</footer>
+<a id="volver" href="#"><i class="fas fa-arrow-circle-up"></i></a>
