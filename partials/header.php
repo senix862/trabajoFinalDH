251eb44c66b870d96cf2ca88ffc33785bf5d1701
denisemartin28 <denmartiin@gmail.com>
2019-09-27 17:31:30 -0300

    header con mejoras

--- partials/header.php
+++ partials/header.php
@@ -1,29 +1,41 @@
+<?php
+  $secciones = [
+    "Home" => "index.php",
+    "Preguntas Frecuentes" => "faq.php",
+    "Contacto" => "contacto.php",
+    "Registrarse" => "registro.php",
+    "Ingresar" => "login.php",
+    "Mi Perfil" => "profile.php"
+  ];
+ ?>
+
+  <link rel="stylesheet" href="css/header.css">
+
 <header>
   <div class="hamburguesa">
     <i class="fas fa-bars"><nav>
       <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="faq.html">Preguntas Frecuentes</a></li>
-        <li><a href="contacto.html">Contacto</a></li>
-        <li><a href="registro.html">Registrarse</a></li>
-        <li><a href="login.html">Login</a></li>
-        <li><a href="profile.html">Mi Perfil</a></li>
+        <?php
+        foreach ($secciones as $seccion => $url) :?>
+          <li><a href="<?php echo $url?>"><?php echo $seccion?></a></li>
+        <?php endforeach; ?>
       </ul>
-    </nav></i>
-
+    </nav>
+  </i>
   </div>
 
   <div class="logo">
-    <a href="index.html"><img src="img/logo.png" alt="logo">
+    <a href="index.php"><img src="img/logo.png" alt="logo">
     <p>
       BLACKBIRD
     </p></a>
   </div>
 
-  <div class="carrito">
-    <div class="carrit">
-      <a href="productos.html"><i class="fas fa-search"></i></a>
-      <a href="carrito.html"><i class="fas fa-shopping-basket"></i></a>
+     <div class="lupa">
+      <a href="productos.php"><i class="fas fa-search"></i></a>
+    </div>
+    <div class="carrito">
+      <a href="carrito.php"><i class="fas fa-shopping-basket"></i></a>
     </div>
   </div>
 
