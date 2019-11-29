23312dbf126f2b2c1383e6cd2674ec8412e25aa5
unknown <vale.s.curcio@gmail.com>
2019-11-29 14:47:37 -0300

    errores spa

--- blackbird/resources/lang/en/passwords.php
+++ blackbird/resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'reset' => 'Su contraseña fue reseteada!',
+    'sent' => 'Te hemos enviado un mail para que recuperes tu contraseña!',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'user' => "No hemos encontrado ningún usuario registrado con ese e-mail.",
     'throttled' => 'Please wait before retrying.',
 
 ];
